8e8e6b9c62436ece93d5f5569ffae5a7f78a3da5
vietbui99 <buiquocviet99@gmail.com>
2023-04-23 00:52:35 -0500

    added a few more functions to play around in main_test.py

--- main_test.py
+++ main_test.py
@@ -4,16 +4,22 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from Model.heat_eq import HeatEquation
 
+#import animation
+import matplotlib.animation as animation
+
 if __name__ == "__main__":
     # make initial condition a function of x
     def initial_condition(iteration, delta_x_step):
-        #return pow(sin(2 * pi * iteration * delta_x_step), 2)
+        return pow(sin(2 * pi * iteration * delta_x_step), 2)
         #return 0
         #return 2 * iteration * delta_x_step + 1
-        return iteration * (1 + iteration)
+        #return iteration * (1 + iteration)
+        #return sin(iteration * delta_x_step) - 6 * sin(4 * iteration * delta_x_step) 
+        #return 20 * sin(2 * pi * iteration * delta_x_step) + 40 * sin(4 * pi * iteration * delta_x_step) - 50 * sin(5 * pi * iteration * delta_x_step)
+        #return 0
 
 
-    L, T, b0t, b1t, beta, delta_t, delta_x = 1, 1, 0, 0, 3, 0.004, 0.1
+    L, T, b0t, b1t, beta, delta_t, delta_x = 1, 1, 0, 0, 1, 0.004, 0.1
     heat_eq = HeatEquation(L, T, b0t, b1t, beta, delta_t, delta_x, initial_condition)
     print(heat_eq.print_tri_diag())
     print(heat_eq.print_initial_condition_vector())
@@ -49,4 +55,22 @@ if __name__ == "__main__":
     plt.ylabel("Time")
 
     plt.show()
+
+    # animate heatmap for the rod
+
+    fig, ax = plt.subplots()
+    pcolor_subplot = ax.pcolor(X, Y, Z, cmap='hot')
+    fig.colorbar(pcolor_subplot, shrink=0.5, aspect=5)
+    plt.title("Heat Equation")
+    plt.xlabel("X")
+    plt.ylabel("Time")
+
+    def animate(i):
+        pcolor_subplot.set_array(Z[i])
+        return pcolor_subplot,
+
+    anim = animation.FuncAnimation(fig, animate, frames=heat_eq.len_t, interval=100, blit=True)
+    plt.show()
+
+
 
