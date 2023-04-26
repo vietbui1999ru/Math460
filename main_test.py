c8ee6298a8c9d8d86d46a26fc589188e3ebca439
vietbui99 <buiquocviet99@gmail.com>
2023-04-26 02:08:45 -0500

    modified main_test to animate and plot graphs in same screen. Added wave equation file WIP

--- main_test.py
+++ main_test.py
@@ -3,6 +3,7 @@ from pylab import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from Model.heat_eq import HeatEquation
+from matplotlib import gridspec
 
 #import animation
 import matplotlib.animation as animation
@@ -19,63 +20,77 @@ if __name__ == "__main__":
         #return 0
 
 
-    L, T, b0t, b1t, beta, delta_t, delta_x = 1, 1, 0, 0, 1, 0.004, 0.1
+    L, T, b0t, b1t, beta, delta_t, delta_x = 1, 1, 0, 0, 0.1, 0.004, 0.1
     heat_eq = HeatEquation(L, T, b0t, b1t, beta, delta_t, delta_x, initial_condition)
     print(heat_eq.print_tri_diag())
     print(heat_eq.print_initial_condition_vector())
-    heat_eq.sigma_checker()
+    print(heat_eq.sigma_checker())
     matrix = heat_eq.return_u_matrix()
-    print(f"temperature at time 250: {matrix[:, 250]}")
+    print(f"temperature at time {heat_eq.len_t - 1}: {matrix[:, heat_eq.len_t - 1]}")
 
     # plot 3d
 
     plt.rcParams['figure.figsize'] = [8, 8]
     plt.rcParams['figure.dpi'] = 100
     plt.rcParams['figure.autolayout'] = False
+    plt.ioff()
     X = np.arange(0, heat_eq.N, 1)
     Y = np.arange(1, heat_eq.len_t + 1, 1)
     X, Y = np.meshgrid(X, Y)
     Z = matrix[X, Y]
     x_scale = (X - X.min()) / (X.max() - X.min())
-    fig = plt.figure()
+    t_scale = (Y - Y.min()) / (Y.max() - Y.min())
 
-    ax = fig.add_subplot(211, projection='3d')
-    surf = ax.plot_surface(x_scale, Y, Z, cmap='hot', linewidth=0, antialiased=False)
+
+    fig = plt.figure(figsize=(8, 8), constrained_layout=True)
+
+    gs = gridspec.GridSpec(2, 2, width_ratios=[2, 1], height_ratios=[1, 1])
+
+    ax1 = fig.add_subplot(gs[0, 0], projection='3d')
+    surf = ax1.plot_surface(x_scale, Y, Z, cmap='hot', linewidth=0, antialiased=False)
     fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.title("Heat Equation")
-    ax.set_xlabel("delta_x")
-    ax.set_ylabel("Time")
-    ax.set_zlabel("Temperature")
+    plt.title("Heat Equation 3D")
+    ax1.set_xlabel("delta_x")
+    ax1.set_ylabel("Time")
+    ax1.set_zlabel("Temperature")
 
     # 2D heatmap
 
-    ax = fig.add_subplot(212)
-    pcolor_subplot = ax.pcolor(x_scale, Y, Z, cmap='hot')
+    ax2 = fig.add_subplot(gs[0, 1])
+    pcolor_subplot = ax2.pcolor(x_scale, Y, Z, cmap='hot')
 
     fig.colorbar(pcolor_subplot, shrink=0.5, aspect=5)
-    plt.title("Heat Equation")
-    ax.set_xlabel("delta_x")
-    ax.set_ylabel("Time")
+    plt.title("Heat Equation 2D")
+    ax2.set_xlabel("delta_x")
+    ax2.set_ylabel("Time")
+    # label for Z
+
+    # animate 2D heatmap
+
+    ax3 = fig.add_subplot(gs[1, :])
+    pcolor_subplot = ax3.pcolor(x_scale, t_scale, Z, cmap='hot')
+
+    fig.colorbar(pcolor_subplot, shrink=0.5, aspect=5)
+    # set height of plot to be smaller
+    ax3.set_box_aspect(0.1)
+
+    plt.title("Animate Heat Equation")
+    ax3.set_xlabel("delta_x")
+    ax3.set_ylabel("Time")
     # label for Z
 
 
+    def animate(i):
+        pcolor_subplot.set_array(matrix[:, i])
+        # update label for y to reflect index i of time step
+        ax3.set_ylabel(f"Time: {i}")
+        return pcolor_subplot,
+
+    # animate for 2D heatmap
+
+    anim = animation.FuncAnimation(fig, animate, frames=heat_eq.len_t, interval=500, blit=True)
     plt.show()
 
-    # animate heatmap for the rod
-
-    #fig, ax = plt.subplots()
-    #pcolor_subplot = ax.pcolor(X, Y, Z, cmap='hot')
-    #fig.colorbar(pcolor_subplot, shrink=0.5, aspect=5)
-    #plt.title("Heat Equation")
-    #plt.xlabel("X")
-    #plt.ylabel("Time")
-
-    #def animate(i):
-    #    pcolor_subplot.set_array(Z[i])
-    #    return pcolor_subplot,
-
-    #anim = animation.FuncAnimation(fig, animate, frames=heat_eq.len_t, interval=100, blit=True)
-    #plt.show()
 
 
 
