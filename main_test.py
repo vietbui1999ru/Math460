2dc5b118403b5507bbe04f50e865421353fd100a
vietbui99 <buiquocviet99@gmail.com>
2023-04-30 02:33:53 -0500

    uncommented some important prints

--- main_test.py
+++ main_test.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     # make initial condition a function of x
     def initial_condition(iteration, delta_x_step):
         #return pow(sin(2 * pi * iteration * delta_x_step), 2)
-        return 0
+        #return 0
         #return 2 * iteration * delta_x_step + 1
         #return iteration * (1 + iteration)
-        #return sin(iteration * delta_x_step) - 6 * sin(4 * iteration * delta_x_step) 
+        return sin(iteration * delta_x_step) - 6 * sin(4 * iteration * delta_x_step) 
         #return 20 * sin(2 * pi * iteration * delta_x_step) + 40 * sin(4 * pi * iteration * delta_x_step) - 50 * sin(5 * pi * iteration * delta_x_step)
 
 
-    L, T, b0t, b1t, beta, delta_t, delta_x = 1, 1, 20, 50, 1, 0.00004, 0.01
+    L, T, b0t, b1t, beta, delta_t, delta_x = 1, 1, 20, 50, 1, 0.004, 0.1
     heat_eq = HeatEquation(L, T, b0t, b1t, beta, delta_t, delta_x, initial_condition)
     #print(heat_eq.print_tri_diag())
     #print(heat_eq.print_initial_condition_vector())
